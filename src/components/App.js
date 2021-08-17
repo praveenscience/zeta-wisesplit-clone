940a6ef28391dc92b349633d5cee058f98a83ca4
Praveen Kumar <praveenscience@gmail.com>
2021-08-17 14:22:39 +0100

    Wired up all the styles.

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,11 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header dark={true} headerClassName="justify-content-center">
+        <Header
+          dark={true}
+          headerClassName="justify-content-center"
+          className="Header"
+        >
           SplitSmart
         </Header>
       </div>

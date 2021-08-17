45a7089f502233c36e0d0f39503cd42ca8b0c4ef
Praveen Kumar <praveenscience@gmail.com>
2021-08-17 14:17:12 +0100

    Added header.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,15 @@
 import React, { Component } from "react";
+import Header from "./Bootstrap/Header";
 
 class App extends Component {
   render() {
-    return <div className="App">Hello ZetaHackers</div>;
+    return (
+      <div className="App">
+        <Header dark={true} headerClassName="justify-content-center">
+          SplitSmart
+        </Header>
+      </div>
+    );
   }
 }
 
